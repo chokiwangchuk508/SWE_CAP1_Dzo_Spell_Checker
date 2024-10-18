c12aa671adcbeb98af5e4a093b9136a7b961f05c
chokiwangchuk508 <wangchukxander@gmail.com>
2024-10-17 23:15:40 -0700

    Add files via upload
    PA 1

---
+++ cleaning.py
@@ -0,0 +1,11 @@
+import re
+
+with open("dzo.txt", "r", encoding="utf-8") as file:
+    content = file.read()
+
+cleaned_content = re.sub(r'[a-zA-Z0-9.,!?\'"()-_]+', '', content)
+
+with open("cleanedzo.txt", "w", encoding="utf-8") as file:
+    file.write(cleaned_content)
+
+print("File cleaned successfully")
