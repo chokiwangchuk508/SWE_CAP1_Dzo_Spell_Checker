3e4eab187875dda6cd2c3fbd4771917322f87077
chokiwangchuk508 <wangchukxander@gmail.com>
2024-10-17 23:14:34 -0700

    Delete csf101-python_exercises.py directory

--- csf101-python_exercises.py/basics/strings.py
+++
@@ -1,8 +0,0 @@
-name = " Choki Wangchuk"
-print(name)
-greeting = 'Hello,'+ name + "!"
-print(greeting)
-greeting_f = f'Helllo, {name}!'
-print(greeting_f)
-name_length = len (name)
-print(name_length)
--- csf101-python_exercises.py/csf_assignment/ex.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-link = 'https://csf101-server-cap1.onrender.com/get/input/336'
-request_file = requests.get(link)
-with open('336.txt', 'wb') as file:
-    file.write(request_file.content)
-
-print("Download 336.txt")
